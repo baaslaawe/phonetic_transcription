9b0532945b8be2292f2442f18c4ed523afcb7892
Yaoyan (Yan) Yang <yan@lingocracy.com>
2014-12-11 19:51:22 +0100

    add test_data

--- trans.py
+++ trans.py
@@ -1,24 +1,27 @@
 # -*- coding: utf-8 -*-
 
 import csv
+import codecs
 
 def get_dict_data():
-    f = open('data/ipa2phonetic.dict')
+    f = codecs.open('data/ipa2phonetic.dict')
     csv_f = csv.DictReader(f, delimiter='\t')
-    dict_hmap = {}
-    for row in csv_f:
-        dict_hmap[row['ipa']] = row
-    return dict_hmap
+    return csv_f
 
 ipa_dict = get_dict_data()
+
+def get_entry(ipa):
+    for entry in get_dict_data():
+        if entry['ipa'].decode('utf-8') == ipa:
+            return entry
+    return None
 
 def get_max_match(raw_str):
     raw_str = raw_str[:3]
     while (raw_str):
-        try:
-            ipa_dict[raw_str]
+        if get_entry(raw_str):
             break
-        except KeyError:
+        else:
             if (len(raw_str) <= 1):
                 break
             raw_str = raw_str[:(len(raw_str)-1)]
@@ -33,28 +36,28 @@ def split(raw_str):
         if (len(raw_str) <= 1):
             segments.append(get_max_match(raw_str))
             break
-
     return segments
 
 def trans(ipa, alpha3):
     segs = split(ipa)
-    out = ""
+    out = u""
     for seg in segs:
-        try:
-            out += ipa_dict[seg][alpha3]
-        except KeyError:
-            # if seg in ["'", "."]:
-            #     out += seg
-            # else:
-            out += "["+seg+"]"
+        if (not seg):
+            continue
+        entry = get_entry(seg)
+        if (entry):
+            out += entry[alpha3].decode('utf-8')
+        else:
+            if seg in [u"ˈ", u"(", u")"]:
+                out += seg
+            else:
+                out += u"[%s]"%seg
     return out
 
 if __name__ == "__main__":
     import sys
     args = sys.argv
     args.pop(0)
-    # print get_dict_data()
-    # for arg in args:
-    #     print trans(arg, 'fra')
-    # print split(args[0])
-    print trans(args[0], 'fra')
+    f = codecs.open('test_data', 'r', encoding='utf8')
+    for line in f.read().split('\n'):
+        print "%s -> %s"%(line, trans(line, 'fra'))
